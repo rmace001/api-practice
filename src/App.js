a6b7e94f5e3a83048ef837537b55d85b863fd44a
Rogelio Macedo <rmace001@ucr.edu>
2022-07-25 22:34:24 -0500

    use class component instead of function, & use fetch w/async/await

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+let photosAPI = "https://jsonplaceholder.typicode.com/photos";
+
+const getPics = async () => {
+  let photosResponse = await fetch(photosAPI);
+  let returnPhotos = await photosResponse.json();
+  return {
+    photos: returnPhotos,
+  };
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount = () => {
+    getPics().then((response) => {
+      console.log(response.photos[0]);
+    });
+  };
+
+  render = () => {
+    return (
+      <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
+        {/* <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+          </p>
+          <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           Learn React
-        </a>
-      </header>
-    </div>
-  );
+          </a>
+        </header> */}
+        <h1>Here is some h1</h1>
+      </div>
+    );
+  };
 }
 
 export default App;
